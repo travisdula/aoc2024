7786625fb06fe3d2e74661ca30bc4f80b2d28f58
Travis A. Dula <travisadula@gmail.com>
2024-12-05 16:23:34 -0600

    Fantomas + shorter code

--- Program.fs
+++ Program.fs
@@ -1,11 +1,12 @@
-﻿let readLines filePath = System.IO.File.ReadLines filePath
-let getData day = "data/" + day |> readLines
-let handleDay day solver = 
+﻿let getData day = "data/" + day |> System.IO.File.ReadLines
+
+let handleDay day solver =
     getData day |> solver |> List.map (printfn "%d") |> ignore
     0
 
 let solve day =
     let handler = handleDay day
+
     match day with
     | "1" -> handler One.solve
     | "2" -> handler Two.solve
@@ -17,5 +18,5 @@ let solve day =
 [<EntryPoint>]
 let main args =
     match args |> Array.toList with
-    | x::[] -> solve x
+    | x :: [] -> solve x
     | _ -> 1
