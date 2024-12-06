8570d9b8a69701156509f275c260d73c0f5d1223
Travis A. Dula <travisadula@gmail.com>
2024-12-06 10:18:56 -0600

    make day 6 modular

--- Solvers/Six.fs
+++ Solvers/Six.fs
@@ -31,50 +31,40 @@ let rowToMapping r row =
 let parse (lines: string seq) =
     lines |> Seq.mapi rowToMapping |> Seq.concat |> Map.ofSeq
 
-let a lab =
-    let rec loop (guard: Guard) (seen: Position Set) =
+let rec loop (guard: Guard) (seen: Guard Set) lab =
+    if Set.contains guard seen then
+        None
+    else
         let ifFwd = moveForward guard
         let (dir, pos) = guard
-        let newSeen = Set.add pos seen
+        let newSeen = Set.add guard seen
 
         match Map.tryFind ifFwd lab with
-        | None -> newSeen
+        | None -> Some newSeen
         | Some o ->
             let newGuard =
                 match o with
                 | '#' -> turn guard
                 | _ -> (dir, ifFwd)
 
-            loop newGuard newSeen
+            loop newGuard newSeen lab
 
+let a lab =
     let startPos = Map.findKey (fun _ v -> v = '^') lab
-    loop (Up, startPos) Set.empty |> Set.count
+
+    match loop (Up, startPos) Set.empty lab with
+    | Some s -> Set.map snd s |> Set.count
+    | None -> failwith "guard could not escape"
 
 let b lab =
-    let rec loop (guard: Guard) (seen: Guard Set) blockedLab =
-        if Set.contains guard seen then
-            true
-        else
-            let ifFwd = moveForward guard
-            let newSeen = Set.add guard seen
-            let (dir, pos) = guard
-
-            match Map.tryFind ifFwd blockedLab with
-            | None -> false
-            | Some o ->
-                let newGuard =
-                    match o with
-                    | '#' -> turn guard
-                    | _ -> (dir, ifFwd)
-
-                loop newGuard newSeen blockedLab
-
     let startPos = Map.findKey (fun _ v -> v = '^') lab
-    let predicate = loop (Up, startPos) Set.empty
 
-    let labWithBlock loc = Map.add loc '#' lab |> predicate
+    let labWithBlock loc =
+        Map.add loc '#' lab |> loop (Up, startPos) Set.empty
 
-    lab |> Map.filter (fun k v -> v = '.' && labWithBlock k) |> Map.count
+    lab
+    |> Map.filter (fun k v -> v = '.' && Option.isNone <| labWithBlock k)
+    |> Map.count
 
 let solve lines =
     let parsed = parse lines
