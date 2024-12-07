c8400cb70e88fee6417fc05b7c63bbbb32d7af83
Travis A. Dula <travisadula@gmail.com>
2024-12-07 00:07:08 -0600

    Day 7 (runner not working right)

---
+++ Solvers/Seven.fs
@@ -0,0 +1,55 @@
+module Seven
+
+let parseLine line =
+    let solution = Seq.takeWhile ((<>) ':') line |> System.String.Concat |> int64
+
+    let parameters =
+        Seq.skipWhile ((<>) ':') line
+        |> Seq.skip 2
+        |> System.String.Concat
+        |> _.Split(' ')
+        |> Seq.ofArray
+        |> Seq.map int64
+
+    (solution, parameters)
+
+let parse (lines: string seq) = lines |> Seq.map parseLine
+
+let concatNumbers a b = string a + string b |> int64
+
+let genericSoln ops eqns =
+    let checkEqn (sol, par) =
+        let folder state elem =
+            state
+            |> Set.toSeq
+            |> Seq.allPairs ops
+            |> Seq.map (fun (f, a) -> f a elem)
+            |> Set.ofSeq
+        let state = Set.singleton <| Seq.head par
+        let source = Seq.tail par
+
+        Seq.fold folder state source |> Set.contains sol
+    eqns |> Seq.filter checkEqn |> Seq.map fst |> Seq.sum
+
+let a =
+    seq {
+        (+)
+        (*)
+    }
+    |> genericSoln
+
+let b =
+    seq {
+        (+)
+        (*)
+        concatNumbers
+    }
+    |> genericSoln
+
+let solve lines =
+    let parsed = parse lines
+    let ansA = a parsed
+    printfn "%A" ansA |> ignore
+    let ansB = b parsed
+    printfn "%A" ansB |> ignore
+    ansA :: ansB :: [] |> List.map int
